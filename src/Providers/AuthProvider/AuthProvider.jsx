e4f84110fd3dc6f43c311d8fe289bf7429104d34
abunaim5 <abunaim140@gmail.com>
2024-05-09 00:56:56 +0600

    manage user with on auth state changed and set current user

--- src/Providers/AuthProvider/AuthProvider.jsx
+++ src/Providers/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { createContext, useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import auth from "../../Firebase/firebase.config";
 
 export const AuthContext = createContext()
@@ -7,12 +7,23 @@ export const AuthContext = createContext()
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+    console.log(user);
 
     const createUser = (email, password) => {
         setLoading(true);
         console.log(email, password);
         return createUserWithEmailAndPassword(auth, email, password)
     }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
 
     const authInfo = {
         user,
