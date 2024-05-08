533cb428977a213ab7e9740e1e3e1a7be56beb3e
abunaim5 <abunaim140@gmail.com>
2024-05-08 12:45:47 +0600

    added footer component

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Pages/Shared/NavBar/NavBar";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const Root = () => {
     return (
         <div>
             <NavBar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
